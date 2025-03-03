ab3b94b1bbec7fea87101f1a4b50670cc79f5021
Purple Winny <winnych8@gmail.com>
2025-03-03 23:40:40 +0200

    .

--- src/MuseumExhibits.Infrastructure/Repositories/ExhibitRepository.cs
+++ src/MuseumExhibits.Infrastructure/Repositories/ExhibitRepository.cs
@@ -68,12 +68,17 @@ namespace MuseumExhibits.Infrastructure.Repostories
             if (!string.IsNullOrWhiteSpace(filter.Name))
                 query = query.Where(e => e.Name.Contains(filter.Name));
 
-            if (filter.CreationExactDate.HasValue)
-                query = query.Where(e => e.CreationExactDate == filter.CreationExactDate);
-            else if (filter.CreationYear.HasValue)
-                query = query.Where(e => e.CreationYear == filter.CreationYear);
-            else if (filter.CreationCentury.HasValue)
-                query = query.Where(e => e.CreationCentury == filter.CreationCentury);
+
+            if (filter.CreationExactDate.HasValue ||
+                filter.CreationYear.HasValue ||
+                filter.CreationCentury.HasValue)
+            {
+                query = query.Where(e =>
+                    (filter.CreationExactDate.HasValue && e.CreationExactDate == filter.CreationExactDate) ||
+                    (filter.CreationYear.HasValue && e.CreationYear == filter.CreationYear) ||
+                    (filter.CreationCentury.HasValue && e.CreationCentury == filter.CreationCentury)
+                );
+            }
 
             if (filter.EntryDate.HasValue)
                 query = query.Where(e => e.EntryDate == filter.EntryDate);
@@ -82,6 +87,7 @@ namespace MuseumExhibits.Infrastructure.Repostories
             {
                 query = query.Where(e => e.Category != null && e.Category.Name.Contains(filter.CategoryName));
             }
+
             return query;
         }
 
@@ -92,9 +98,27 @@ namespace MuseumExhibits.Infrastructure.Repostories
                 case "name":
                     query = filter.Descending ? query.OrderByDescending(e => e.Name) : query.OrderBy(e => e.Name);
                     break;
-                case "creationexactdate":
-                    query = filter.Descending ? query.OrderByDescending(e => e.CreationExactDate) : query.OrderBy(e => e.CreationExactDate);
+
+                case "creation":
+                    query = filter.Descending
+                        ? query.OrderByDescending(e =>
+                              e.CreationExactDate.HasValue
+                                  ? e.CreationExactDate.Value
+                                  : e.CreationYear.HasValue
+                                      ? new DateOnly(e.CreationYear.Value, 1, 1)
+                                      : e.CreationCentury.HasValue
+                                          ? new DateOnly((e.CreationCentury.Value - 1) * 100 + 1, 1, 1)
+                                          : DateOnly.MaxValue)
+                        : query.OrderBy(e =>
+                              e.CreationExactDate.HasValue
+                                  ? e.CreationExactDate.Value
+                                  : e.CreationYear.HasValue
+                                      ? new DateOnly(e.CreationYear.Value, 1, 1)
+                                      : e.CreationCentury.HasValue
+                                          ? new DateOnly((e.CreationCentury.Value - 1) * 100 + 1, 1, 1)
+                                          : DateOnly.MaxValue);
                     break;
+
                 case "entrydate":
                 default:
                     query = filter.Descending ? query.OrderByDescending(e => e.EntryDate) : query.OrderBy(e => e.EntryDate);
